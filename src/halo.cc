bf227e9b1959685c99b10ff50d2bcc010da6473f
lthebault <loic.thebault@prism.uvsq.fr>
2015-03-04 18:27:41 +0100

    Still debugging the C version of MPI comms

--- src/halo.cc
+++ src/halo.cc
@@ -25,70 +25,56 @@ void halo_exchange (double *prec, double *buffer, int *intfIndex, int *intfNodes
                     int *neighborList, int nbNodes, int nbIntf, int nbIntfNodes,
                     int operatorDim, int mpiRank)
 {
-    double *bufferSend = new double [nbIntfNodes*DIM_NODE*operatorDim];
-    double *bufferRecv = new double [nbIntfNodes*DIM_NODE*operatorDim];
-    int source, dest, size, tag, node1, node2 = 0;
+    double *bufferSend = new double [nbIntfNodes*operatorDim];
+    double *bufferRecv = new double [nbIntfNodes*operatorDim];
+    int source, dest, size, tag, node1, node2 = -1;
 
     // Initializing reception from adjacent domains
-    printf ("C++\n");
     for (int i = 0; i < nbIntf; i++) {
-        node1  = node2;
+        node1  = node2 + 1;
         node2  = intfIndex[i+1];
         size   = (node2 - node1) * operatorDim;
         source = neighborList[i] - 1;
-        tag    = neighborList[i] + 99;
-        MPI_Irecv (&(bufferRecv[node1]), size,
+        tag    = neighborList[i] + 100;
+        MPI_Irecv (&(bufferRecv[node1*operatorDim]), size,
                    MPI_DOUBLE_PRECISION, source, tag, MPI_COMM_WORLD,
                    &(neighborList[2*nbIntf+i]));
     }
 
     // Buffering local data
-    node2 = 0;
+    node2 = -1;
     for (int i = 0; i < nbIntf; i++) {
-        node1  = node2;
-        node2  = intfIndex[i+1];
+        node1 = node2 + 1;
+        node2 = intfIndex[i+1];
         for (int j = 0; j < operatorDim; j++) {
             for (int k = node1; k < node2; k++) {
-                int tmpNode = intfNodes[k] - 1;
+                int tmpNode = intfNodes[k];
                 bufferSend[k*operatorDim+j] = prec[tmpNode*operatorDim+j];
-                if (i == 0 && j == 0 && k == node1) {
-                    printf ("%d : prec[%d*%d+%d] = %f\n", mpiRank, tmpNode,
-                            operatorDim, j, prec[tmpNode*operatorDim+j]);
-                    for (int z = 0; z < operatorDim*DIM_NODE*nbNodes; z++)
-                        if (prec[z] >= 2.1389718111366E-003 &&
-                            prec[z] <= 2.1389718111367E-003)
-                            printf ("%d : valeur %f atteinte pour z = %d\n", mpiRank,
-                                    prec[z], z); //68086
-                }
             }
         }
     }
 
     // Sending local data to adjacent domains
-    node2 = 0;
+    node2 = -1;
     for (int i = 0; i < nbIntf; i++) {
-        node1 = node2;
+        node1 = node2 + 1;
         node2 = intfIndex[i+1];
         size  = (node2 - node1) * operatorDim;
         dest  = neighborList[i] - 1;
         tag   = mpiRank + 100;
-//        printf ("%d :\t%d\t%d\t%d\t%d\t%d\n", mpiRank, node1, node2, size, dest, tag);
-        MPI_Send (&(bufferSend[node1]), size, MPI_DOUBLE_PRECISION, dest, tag,
-                  MPI_COMM_WORLD);
+        MPI_Send (&(bufferSend[node1*operatorDim]), size, MPI_DOUBLE_PRECISION, dest,
+                  tag, MPI_COMM_WORLD);
     }
 
     // Waiting incoming data
-    node2 = 0;
     for (int i = 0; i < nbIntf; i++) {
-        node1 = node2;
-        node2 = intfIndex[i+1];
         MPI_Wait (&(neighborList[2*nbIntf+i]), MPI_STATUS_IGNORE);
     }
 
     // Assembling local and incoming data
-    node2 = 0;
+    node2 = -1;
     for (int i = 0; i < nbIntf; i++) {
-        node1  = node2;
+        node1  = node2 + 1;
         node2  = intfIndex[i+1];
         for (int j = 0; j < operatorDim; j++) {
             for (int k = node1; k < node2; k++) {
