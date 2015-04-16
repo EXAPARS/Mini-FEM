5f01117e3ac75a10a7cb910d3be94f654f02005f
lthebault <loic.thebault@prism.uvsq.fr>
2015-04-16 23:02:26 +0200

    Modify the anselm exe scripts to handle gaspi version

--- exe/mesures_miniFEM_anselm.sh
+++ exe/mesures_miniFEM_anselm.sh
@@ -1,95 +1,113 @@
-#!/bin/sh
+#!/bin/bash
 
 # Set the parameters
-MAX_CORES=240
 EXE_DIR=$HOME/lthebaul/Mini-FEM/exe
-MACHINE_FILE=$EXE_DIR/machine_file
-EXE_FILE=$EXE_DIR/env_file.sh
-TEST_CASE=LM6
+MACHINE_FILE=$EXE_DIR/GASPI_machine_file
+EXE_FILE=$EXE_DIR/GASPI_exe.sh
+TEST_CASE=EIB
 VECTOR_LENGTH=AVX
-NB_ITERATIONS=1
+NB_ITERATIONS=50
 
 # Go to the appropriate directory, exit on failure
 cd $EXE_DIR || exit
 
-# Load the required modules
-module load gpi2/1.1.1 PrgEnv-intel/14.0.1
-
-# Create the machine and the environment file
-cat $PBS_NODEFILE | cut -d'.' -f1 > $MACHINE_FILE
-echo "#!/bin/sh" > $EXE_FILE
-echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$LD_LIBRARY_PATH" >> $EXE_FILE
-chmod +x $EXE_FILE
-
-for OPERATOR in 'ela' #'lap'
+for DISTRI in 'GASPI' 'XMPI'
 do
-    for PART_SIZE in 200 #50 200 500
+    # Set the environment
+    if [ $DISTRI == "GASPI" ]; then
+        module load PrgEnv-intel/14.0.1 gpi2/1.1.1
+        cat $PBS_NODEFILE | cut -d'.' -f1 > $MACHINE_FILE
+    else
+        module load PrgEnv-intel/14.0.1 impi/4.1.1.036
+    fi
+
+    for SHARED in 'CILK' #'OMP'
     do
-        export elemPerPart=$PART_SIZE
-   	    echo "$TEST_CASE $OPERATOR, $PART_SIZE elements max per partition"
-
-        for VERSION in 'REF' #'COLORING_OMP' 'DC' 'DC_HYBRID'
+        for VERSION in 'REF' #'DC' 'DC_HYBRID' 'COLORING_OMP'
         do
-            BINARY=$EXE_DIR/bin/miniFEM_GASPI_CILK_$VERSION
-            OUTPUT_FILE=$EXE_DIR/stdout_$VERSION\_$NB_NODES
-            echo "$BINARY $TEST_CASE $OPERATOR $NB_ITERATIONS" >> $EXE_FILE
-            echo -e "\n$VERSION"
+            echo -e "\n$VERSION version using $DISTRI and $SHARED"
+            BINARY=$EXE_DIR/bin/miniFEM_$DISTRI\_$SHARED\_$VERSION
 
-            for NB_PROCESS in 1 #4 8 12 16 32
+            for OPERATOR in 'ela' 'lap'
             do
-                for NB_THREADS in 1 #2 4 8 12 16
+                # Create the GASPI execution script
+                if [ $DISTRI == "GASPI" ]; then
+                    echo "#!/bin/sh" > $EXE_FILE
+                    echo "cd $EXE_DIR" >> $EXE_FILE
+                    echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH" >> $EXE_FILE
+                    echo "$BINARY $TEST_CASE $OPERATOR $NB_ITERATIONS" >> $EXE_FILE
+                    chmod +x $EXE_FILE
+                fi
+
+                for PART_SIZE in 200 #50 200 500
                 do
-                    let "nbCores=$NB_PROCESS*$NB_THREADS"
-                    if [ "$nbCores" -gt "$MAX_CORES" ]; then break; fi
-                    if [ $VERSION == 'REF' ] && [ $NB_THREADS -gt 1 ]; then break; fi
+                    export elemPerPart=$PART_SIZE
+               	    echo -e "\n$OPERATOR operator, $PART_SIZE elements max per partition"
 
-                    export CILK_NWORKERS=$NB_THREADS
-                    export OMP_NUM_THREADS=$NB_THREADS
-                    echo "$NB_PROCESS processus, $NB_THREADS threads"
+                    for NB_PROCESS in 16 #1 4 8 12 16
+                    do
+                        for NB_THREADS in 1 #2 4 8 12 16
+                        do
+                            let "nbCores=$NB_PROCESS*$NB_THREADS"
+                            if [ "$nbCores" -gt "$MAX_CORES" ]; then break; fi
+                            if [ $VERSION == 'REF' ] && [ $NB_THREADS -gt 1 ]; then break; fi
 
-       	            #mpirun -np $NB_PROCESS $BINARY $TEST_CASE $OPERATOR $NB_ITERATIONS > $OUTPUT_FILE
-                    gaspi_run -m $MACHINE_FILE $EXE_FILE  > $OUTPUT_FILE
-
-       	            matrixASM=0
-       	            precond=0
-                    firstASM=1
-                    firstPrecond=1
-       	            while read line
-       	            do
-       	            	echo $line | grep -q "Matrix assembly"
-       	            	if [ $? == 0 ]; then
-       	            		tmpVar=`echo $line | cut -d " " -f5`
-                            echo $tmpVar >> miniFEM_$TEST_CASE\_$NB_NODES\_$OPERATOR\_$PART_SIZE\_$VERSION\_$NB_PROCESS\_$NB_THREADS.ASM
-                            if [ $firstASM == 0 ]; then
-       	                        let "matrixASM=$matrixASM+$tmpVar"
+                            echo "$NB_PROCESS processus, $NB_THREADS threads"
+                            if [ $SHARED == "CILK" ]; then
+                                export CILK_NWORKERS=$NB_THREADS
+                            else
+                                export OMP_NUM_THREADS=$NB_THREADS
                             fi
-                            firstASM=0
-       	            	fi
-       	            	echo $line | grep -q "Preconditioner"
-       	            	if [ $? == 0 ]; then
-       	            		tmpVar=`echo $line | cut -d " " -f4`
-                            echo $tmpVar >> miniFEM_$TEST_CASE\_$NB_NODES\_$OPERATOR\_$PART_SIZE\_$VERSION\_$NB_PROCESS\_$NB_THREADS.prec
-                            if [ $firstPrecond == 0 ]; then
-       	            		    let "precond=$precond+$tmpVar"
+
+                            OUTPUT_FILE=$EXE_DIR/stdout_$TEST_CASE\_$NB_NODES\_$DISTRI\_$SHARED\_$VERSION\_$OPERATOR\_$PART_SIZE\_$NB_PROCESS\_$NB_THREADS
+
+                            if [ $DISTRI == "GASPI" ]; then
+                                gaspi_run -m $MACHINE_FILE $EXE_FILE > $OUTPUT_FILE
+                            else
+       	                        mpirun -np $NB_PROCESS $BINARY $TEST_CASE $OPERATOR $NB_ITERATIONS > $OUTPUT_FILE
                             fi
-                            firstPrecond=0
-       	            	fi
-       	            done < $OUTPUT_FILE
-       	            let "matrixASM=$matrixASM/($NB_ITERATIONS-1)"
-       	            let "precond=$precond/($NB_ITERATIONS-1)"
-                    let "total=$matrixASM+$precond"
 
-       	            echo "Matrix assembly         : $matrixASM cycles"
-       	            echo "Preconditioner creation : $precond cycles"
-       	            echo "Total                   : $total cycles"
+       	                    matrixASM=0
+       	                    precond=0
+                            firstASM=1
+                            firstPrecond=1
+       	                    while read line
+       	                    do
+       	                    	echo $line | grep -q "Matrix assembly"
+       	                    	if [ $? == 0 ]; then
+       	                    		tmpVar=`echo $line | cut -d " " -f5`
+                                    echo $tmpVar >> ASM_$TEST_CASE\_$NB_NODES\_$DISTRI\_$SHARED\_$VERSION\_$OPERATOR\_$PART_SIZE\_$NB_PROCESS\_$NB_THREADS
+                                    if [ $firstASM == 0 ]; then
+       	                                let "matrixASM=$matrixASM+$tmpVar"
+                                    fi
+                                    firstASM=0
+       	                    	fi
+       	                    	echo $line | grep -q "Preconditioner"
+       	                    	if [ $? == 0 ]; then
+       	                    		tmpVar=`echo $line | cut -d " " -f4`
+                                    echo $tmpVar >> Prec_$TEST_CASE\_$NB_NODES\_$DISTRI\_$SHARED\_$VERSION\_$OPERATOR\_$PART_SIZE\_$NB_PROCESS\_$NB_THREADS
+                                    if [ $firstPrecond == 0 ]; then
+       	                    		    let "precond=$precond+$tmpVar"
+                                    fi
+                                    firstPrecond=0
+       	                    	fi
+       	                    done < $OUTPUT_FILE
+       	                    let "matrixASM=$matrixASM/($NB_ITERATIONS-1)"
+       	                    let "precond=$precond/($NB_ITERATIONS-1)"
+                            let "total=$matrixASM+$precond"
 
+       	                    echo "Matrix assembly         : $matrixASM cycles"
+       	                    echo "Preconditioner creation : $precond cycles"
+       	                    echo "Total                   : $total cycles"
+                        done
+                    done
                 done
             done
         done
-        echo
     done
+    if [ $DISTRI == "GASPI" ]; then
+        rm $MACHINE_FILE $EXE_FILE
+    fi
 done > $EXE_DIR/miniFEM_$TEST_CASE\_$NB_NODES.log
-
-rm $MACHINE_FILE $EXE_FILE
 
 exit
--- exe/run_anselm.sh
+++ exe/run_anselm.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
-qsub -q qexp -l select=1:ncpus=4:mpiprocs=4:cpu_freq=24,walltime=00:02:00 \
-     -v NB_NODES=1,MAX_CORES=4 ./mesures_miniFEM_anselm.sh
+qsub -q qexp -l select=1:ncpus=16:mpiprocs=16:cpu_freq=24,walltime=01:00:00 \
+     -v NB_NODES=1,MAX_CORES=16 ./mesures_miniFEM_anselm.sh
 
-#qsub -A IT4I-3-4 -q qprod -l select=4:ncpus=16:cpu_freq=24,walltime=00:30:00 \
+#qsub -A DD-15-6 -q qprod -l select=1:ncpus=16:cpu_freq=24,walltime=02:30:00 \
+#     -v NB_NODES=1,MAX_CORES=16 ./mesures_miniFEM_anselm.sh
+#
+#qsub -A DD-15-6 -q qprod -l select=4:ncpus=16:cpu_freq=24,walltime=00:30:00 \
 #     -v NB_NODES=4,MAX_CORES=64 ./mesures_miniFEM_anselm.sh
 #
-#qsub -A IT4I-3-4 -q qprod -l select=8:ncpus=16:cpu_freq=24,walltime=00:30:00 \
+#qsub -A DD-15-6 -q qprod -l select=8:ncpus=16:cpu_freq=24,walltime=00:30:00 \
 #     -v NB_NODES=8,MAX_CORES=128 ./mesures_miniFEM_anselm.sh
 #
-#qsub -A IT4I-3-4 -q qprod -l select=12:ncpus=16:cpu_freq=24,walltime=00:30:00 \
+#qsub -A DD-15-6 -q qprod -l select=12:ncpus=16:cpu_freq=24,walltime=00:30:00 \
 #     -v NB_NODES=12,MAX_CORES=192 ./mesures_miniFEM_anselm.sh
 #
-#qsub -A IT4I-3-4 -q qprod -l select=16:ncpus=16:cpu_freq=24,walltime=00:30:00 \
+#qsub -A DD-15-6 -q qprod -l select=16:ncpus=16:cpu_freq=24,walltime=00:30:00 \
 #     -v NB_NODES=16,MAX_CORES=256 ./mesures_miniFEM_anselm.sh
 #
-#qsub -A IT4I-3-4 -q qprod -l select=32:ncpus=16:cpu_freq=24,walltime=00:30:00 \
+#qsub -A DD-15-6 -q qprod -l select=32:ncpus=16:cpu_freq=24,walltime=00:30:00 \
 #     -v NB_NODES=32,MAX_CORES=512 ./mesures_miniFEM_anselm.sh
