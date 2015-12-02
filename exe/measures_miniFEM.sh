f158ef9bc9f33b490b666b79a30c81723ea90ddb
lthebault <loic.thebault@prism.uvsq.fr>
2015-12-02 16:48:12 +0100

    Change measure miniFEM script

--- exe/measures_miniFEM.sh
+++ exe/measures_miniFEM.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 # Set the parameters
-EXE_DIR=$HOME/lthebaul/Mini-FEM/exe
+EXE_DIR=$HOME/Dassault/Mini-FEM/exe
 EXE_FILE=$EXE_DIR/GASPI_exe_$NB_NODES.sh
 MACHINE_FILE=$EXE_DIR/GASPI_machine_file_$NB_NODES
 TEST_CASE=EIB
 VECTOR_LENGTH=AVX
-NB_ITERATIONS=1
+NB_ITERATIONS=50
 
 # Go to the appropriate directory, exit on failure
 cd $EXE_DIR || exit
 
-for VERSION in 'DC' #'DC_VEC' #'REF' 'COLORING_OMP'
+for VERSION in 'REF_BulkSynchronous' 'DC_BulkSynchronous_TreeCreation' \
+                                     'DC_MultithreadedComm_TreeCreation'
 do
-    for DISTRI in 'GASPI' #'XMPI'
+    for DISTRI in 'XMPI' 'GASPI'
     do
         # Set the environment
-        module load intel/2015b
+        module load CMake/3.0.0-intel-2015b
         if [ $DISTRI == "GASPI" ]; then
             export PATH=$PATH:$HOME/Programs/GPI-2/bin
         fi
@@ -26,11 +27,6 @@ do
             BINARY=$EXE_DIR/bin/miniFEM_$VERSION\_$DISTRI\_$SHARED
             if [ $VERSION == "DC_VEC" ]; then
                 BINARY=$BINARY\_$VECTOR_LENGTH
-            fi
-            if [ $DISTRI == "GASPI" ]; then
-                BINARY=$BINARY\_MultithreadedComm_TreeCreation
-            else
-                BINARY=$BINARY\_BulkSynchronous_TreeCreation
             fi
 
             for OPERATOR in 'ela' #'lap'
@@ -48,23 +44,29 @@ do
                 do
                     export elemPerPart=$PART_SIZE
 
-                    #for NB_PROCESS in $NB_NODES #1 4 8 12 16 32 64 128 256 512
-                    for NB_PROCESS in 4 #4 8 #16 32 64 128 256 512
+                    #for NB_PROCESS in 1 4 8 16 #32 64 128 256 512
+                    for NB_PROCESS in $NB_NODES
                     do
-                        # Create the GASPI machine file
-                        if [ $DISTRI == "GASPI" ]; then
-                            cat $PBS_NODEFILE | head -n $NB_PROCESS | cut -d'.' -f1 > $MACHINE_FILE
-                        fi
 
-                        for NB_THREADS in 4 #1 4 8 12 16
+                        for NB_THREADS in 1 4 8 12 16 24
                         do
-                            let "nbCores=$NB_PROCESS*$NB_THREADS"
-#                            if [ "$nbCores" -gt "$MAX_CORES" ]; then break; fi
-                            if [ $VERSION == 'REF' ] && [ $NB_THREADS -gt 1 ]; then break; fi
+                            #let "nbCores=$NB_PROCESS*$NB_THREADS"
+                            let "nbCores=$NB_NODES*$NB_THREADS"
+                            if [ "$nbCores" -gt "$MAX_CORES" ]; then break; fi
+                            #if [ $VERSION == 'REF' ] && [ $NB_THREADS -gt 1 ]; then break; fi
+                            if [ $VERSION == 'REF_BulkSynchronous' ]; then
+                                NB_PROCESS=$nbCores
+                                NB_THREADS=1
+                            fi
                             if [ $SHARED == "CILK" ]; then
                                 export CILK_NWORKERS=$NB_THREADS
                             else
                                 export OMP_NUM_THREADS=$NB_THREADS
+                            fi
+
+                            # Create the GASPI machine file
+                            if [ $DISTRI == "GASPI" ]; then
+                                cat $PBS_NODEFILE | head -n $NB_PROCESS | cut -d'.' -f1 > $MACHINE_FILE
                             fi
 
                             OUTPUT_FILE=$EXE_DIR/stdout_$TEST_CASE\_$NB_NODES\_$VERSION\_$DISTRI\_$SHARED\_$OPERATOR\_$PART_SIZE\_$NB_PROCESS\_$NB_THREADS
--- exe/run_salomon.sh
+++ exe/run_salomon.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-qsub -q qexp -l select=1:ncpus=24:mpiprocs=24,walltime=01:00:00 \
-     -l cpu_turbo_boost=0 -v NB_NODES=1,MAX_CORES=24 ./measures_miniFEM.sh
-
-#qsub -A DD-15-6 -q qprod -l select=1:ncpus=24:mpiprocs=1,walltime=01:00:00 \
+#qsub -q qexp -l select=1:ncpus=24:mpiprocs=24,walltime=01:00:00 \
 #     -l cpu_turbo_boost=0 -v NB_NODES=1,MAX_CORES=24 ./measures_miniFEM.sh
 
-#qsub -A DD-15-6 -q qprod -l select=4:ncpus=24:mpiprocs=1,walltime=00:30:00 \
-#     -l cpu_turbo_boost=0 -v NB_NODES=4,MAX_CORES=96 ./measures_miniFEM.sh
+qsub -A DD-15-6 -q qprod -l select=1:ncpus=24:mpiprocs=24,walltime=01:00:00 \
+     -l cpu_turbo_boost=0 -v NB_NODES=1,MAX_CORES=24 ./measures_miniFEM.sh
+
+qsub -A DD-15-6 -q qprod -l select=4:ncpus=24:mpiprocs=96,walltime=00:30:00 \
+     -l cpu_turbo_boost=0 -v NB_NODES=4,MAX_CORES=96 ./measures_miniFEM.sh
 
 #qsub -A DD-15-6 -q qprod -l select=8:ncpus=24:mpiprocs=1,walltime=00:30:00 \
 #     -l cpu_turbo_boost=0 -v NB_NODES=8,MAX_CORES=192 ./measures_miniFEM.sh
